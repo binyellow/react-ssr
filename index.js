c7b489a6bfedab870362980d227f2f759b1d10ec
黄斌 <bin.huang02@hand-china.com>
2019-02-18 23:04:35 +0800

    update: 提交修改2-18

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
+import AsyncHome from './Home';
+import AsyncHello from './Hello';
+
 const Loading = () => <div>Loading...</div>;
 const LazyLoad = loader => Loadable({
   loader,
   loading:Loading,
 })
-const AsyncHome = LazyLoad(()=>import(/* webpackChunkName: 'Home' */'./Home'));
-const AsyncHello = LazyLoad(()=>import(/* webpackChunkName: 'Hello' */'./Hello'));
+// const AsyncHome = LazyLoad(()=>import(/* webpackChunkName: 'Home' */'./Home'));
+// const AsyncHello = LazyLoad(()=>import(/* webpackChunkName: 'Hello' */'./Hello'));
 
-class A extends Component {
+export default class A extends Component {
   render() {
     return (
       <div>
@@ -25,7 +28,7 @@ class A extends Component {
   }
 }
 
-ReactDOM.render(<A/>, document.getElementById('app'))
-if (module.hot) {
-  module.hot.accept();
-}
+// ReactDOM.render(<A/>, document.getElementById('app'))
+// if (module.hot) {
+//   module.hot.accept();
+// }
